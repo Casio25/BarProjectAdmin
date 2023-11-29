2cb9b1b5eadc00fdfb9ff8afd0cad19bb58cfe41
casio25 <mishakolomiets355@gmail.com>
2023-11-29 18:35:03 +0200

    node reinstall

--- bar_project_admin/src/app/[locale]/components/changeLanguage.tsx
+++ bar_project_admin/src/app/[locale]/components/changeLanguage.tsx
@@ -4,9 +4,25 @@ import { Link } from "@/navigation"
 export const ChangeLanguage = () => {
     const pathname = usePathname()
     return (
-        <div>
+        <div className="flex justify-center">
+            <div className="bg-white text-black">
+                <button>
+                    country flag
+                </button>
+
+                <input name="country" type="text"></input>
+                <ul>
+                    <Link href={pathname} locale="ua">
+                        <li>UKR</li>
+                    </Link>
+                    <Link href={pathname} locale="ua">
+                        <li>ENG</li>
+                    </Link>
+                </ul>
+            </div>
             <Link href={pathname} locale="ua">UKR</Link>
-            <Link href={pathname} locale="en">ENG</Link>
+            <Link href={pathname} locale="ua">ENG</Link>
+
         </div>
     )
 }
