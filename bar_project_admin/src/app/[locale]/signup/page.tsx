77488cc6b0782f2d755b4c0bb46e6dd3a2768473
casio25 <mishakolomiets355@gmail.com>
2024-06-29 09:54:18 +0300

    sign_in rename

--- bar_project_admin/src/app/[locale]/signup/page.tsx
+++ bar_project_admin/src/app/[locale]/signup/page.tsx
@@ -16,7 +16,7 @@ const SignUpPage = () => {
         <div className="min-h-screen flex flex-col items-center justify-center bg-violet-900">
             <div className='mt-10 w-full flex flex-col items-center max-w-md p-6 bg-white rounded-lg shadow-lg'>
                 <button className="mr-auto border-2 border-violet-500 flex rounded-full active:ring-4 ">
-                    <Link href="./signin"><ArrowBackIcon /></Link>
+                    <Link href="./sign_in"><ArrowBackIcon /></Link>
                 </button>
                 
                 <h2 className='text-center font-semibold text-lg'>{t("signup title")}</h2>
