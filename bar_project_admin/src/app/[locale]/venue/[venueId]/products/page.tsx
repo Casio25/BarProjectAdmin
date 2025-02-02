74b91ececd7a7cf2cc3d5c21f439c8c02e76d615
Casio25 <mishakolomiets355@gmail.com>
2025-02-02 20:24:40 +0200

    use client fix

--- bar_project_admin/src/app/[locale]/venue/[venueId]/products/page.tsx
+++ bar_project_admin/src/app/[locale]/venue/[venueId]/products/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useState } from 'react'
 import SideNavBar from '../../../components/sideNavBar'
 import { GetProductsMenu } from '../../../components/getProductsMenu';
